a2ba9a2affd002d0bb8a74a1161983fde28bcc92
antontx <tarisjan@gmail.com>
2023-06-09 00:01:19 +0200

    another spelling error in the model name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from torch import cuda, bfloat16
 from transformers import AutoModelForCausalLM
 
 device = f"cuda:{cuda.current_device()}" if cuda.is_available() else "cpu"
-model_name = "mosaicml/mpt-7b-instructions"
+model_name = "mosaicml/mpt-7b-instruct"
 
 model = AutoModelForCausalLM.from_pretrained(model_name,
                                              trust_remote_code=True,
