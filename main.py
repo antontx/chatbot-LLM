3fdf1daaeec96f8a6c5e81432b87400e03fa6b69
antontx <tarisjan@gmail.com>
2023-06-08 23:57:43 +0200

    model loading

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from torch import cuda, bfloat16
+from transformers import AutoModelForCausalLM
+
+device = f"cuda:{cuda.current_device()}" if cuda.is_available() else "cpu"
+model_name = "mosaiccml/mpt-7b-instructions"
+
+model = AutoModelForCausalLM.from_pretrained(model=model_name,
+                                             trust_remote_code=True,
+                                             torch_dtype=bfloat16,
+                                             max_seq_len=2048)
